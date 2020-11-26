00b6d7603e3efeb66dfe13901bf75275c599ca55
soporteGT <soporte@gestech.com.co>
2020-11-26 00:03:58 -0500

    Ajuste

--- src/GeneradorVertebrados.java
+++ src/GeneradorVertebrados.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 public class GeneradorVertebrados {
     public static Vertebrado[] generarArrayVertebrados (){
         Vertebrado[] miVertebrados = new Vertebrado[4];
@@ -9,16 +8,3 @@ public class GeneradorVertebrados {
         return miVertebrados;
     }
 }
-=======
-public class GeneradorVertebrados {
-    public static Vertebrado[] generarArrayVertebrados (){
-        Vertebrado[] miVertebrados = new Vertebrado[4];
-        miVertebrados[0] = new Vertebrado("Camello", "Mamifero"); 
-        miVertebrados[1] = new Vertebrado("Vaca", "Mamifero"); 
-        miVertebrados[2] = new Vertebrado("Paloma", "Ave"); 
-        miVertebrados[3] = new Vertebrado("Tiburón", "Pez"); 
-        
-        return miVertebrados;
-    }
-}
->>>>>>> 5a3e995a20748e2a48eba4c3a24105a82f00f216
